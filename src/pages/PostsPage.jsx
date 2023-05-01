c17592b5cc82770907f1df8d26eb02ed547b0102
Vanya Mark <markova.vanya@gmail.com>
2023-05-02 01:27:46 +0200

    added create new post form which consoles logs but is not connecting to Mongo - to keep working

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import AddPostPage from "../components/AddPost";
+
+const API_URL = "http://localhost:5005";
 
 const PostsPage = () => {
-  return (
-    <div>PostsPage</div>
-  )
-}
+  const [posts, setPosts] = useState([]);
 
-export default PostsPage
+  const getAllPosts = () => {
+    const storedToken = localStorage.getItem("authToken");
+    axios
+      .get(`${API_URL}/api/projects`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((response) => setPosts(response.data))
+      .catch((error) => console.log(error));
+  };
+
+  // We set this effect will run only once, after the initial render
+  // by setting the empty dependency array - []
+  useEffect(() => {
+    getAllPosts();
+  }, []);
+
+  return (
+    <div>
+      <AddPostPage getAllPosts ={getAllPosts}/>
+    </div>
+  );
+};
+
+export default PostsPage;
