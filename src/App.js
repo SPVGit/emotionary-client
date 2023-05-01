8d3417ae66f396b0cdecf217834c32dfd14f8c54
Vanya Mark <markova.vanya@gmail.com>
2023-05-01 14:16:29 +0200

    created home signup login posts and singlepost pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import {Route, Routes} from "react-router-dom"
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import PostsPage from "./pages/PostsPage";
+import SinglePostPage from "./pages/SinglePostPage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path= "/posts" element={<PostsPage /> } />
+        <Route path="/posts/:postid" element={<SinglePostPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="Navbar">
+      <ul>
+        {/* <Link to="/"> Home </Link> */}
+        {/* <Link to="/about"> About </Link> */}
+        {/* <Link to="/projects"> Projects </Link> */}
+        
+        {/*    ADD    */}
+        <NavLink to="/" className={({ isActive }) => isActive ? "selected" : ""}>
+          Home
+        </NavLink>
+        
+        <NavLink 
+          to="/signup" 
+          className={({ isActive }) => isActive ? "selected" : ""}
+         >
+          Sign up
+        </NavLink>
+        
+        <NavLink 
+          to="/login" 
+          className={({ isActive }) => isActive ? "selected" : ""}
+        >
+          Login
+        </NavLink>
+        
+      </ul>
+      
+    </nav>
   )
 }
 
