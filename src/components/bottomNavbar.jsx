21e4395e8b6e8fb22f61cdd1003d22315ccabab7
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2023-05-07 17:31:18 +0200

    ui2 progress

--- src/components/bottomNavbar.jsx
+++ src/components/bottomNavbar.jsx
@@ -9,26 +9,47 @@ export default function BottomNavbar() {
   return (
     <Navbar
       variant="dark"
-      expand="lg">
-      <Container>
-        <Nav className="'me-auto">
-          <>
-            <Nav.Link
-              as={NavLink}
-              to="/addpost">
-              Emotion button
-            </Nav.Link>
-            <Nav.Link
-              as={NavLink}
-              to="/chat">
-              Chat
-            </Nav.Link>
-            <Nav.Link
-              as={NavLink}
-              to="/calendar">
-              Calendar
-            </Nav.Link>
-          </>
+      expand="lg"
+      fixed="bottom">
+      <Container className="d-flex justify-content-center ">
+        <Nav className="'me-auto flex-row ">
+          <Nav.Link
+            className="m-4 p-2"
+            as={NavLink}
+            to="/calendar">
+            <img
+              className="icons"
+              src="/calendar-date-fill.svg"
+              alt="calendar-icon"
+              width="60px"
+              height="40px"
+            />
+          </Nav.Link>
+          <Nav.Link
+            className="m-5 p-2"
+            as={NavLink}
+            to="/addpost">
+            <img
+              className="icons"
+              src="/plus-circle-fill.svg"
+              alt="plus-icon"
+              width="90px"
+              height="90px"
+            />
+          </Nav.Link>
+
+          <Nav.Link
+            className="m-4 p-2"
+            as={NavLink}
+            to="/chat">
+            <img
+              className="icons"
+              src="/chat-right-text-fill.svg"
+              alt="icon-chat"
+              width="60px"
+              height="60px"
+            />
+          </Nav.Link>
         </Nav>
       </Container>
     </Navbar>
