9805292d053bef0194328eb7e0b8be53f7e19932
Subarna Paul Vignarajah <subarnapaul@rocketmail.com>
2023-05-12 00:35:14 +0100

    sorted auth problem

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,39 @@
 import React from "react"
 import { Container } from "react-bootstrap"
 import { useNavigate } from "react-router-dom"
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
 
 const HomePage = () => {
+  const { isLoggedIn,user,therapist} = useContext(AuthContext);
   const navigate = useNavigate()
 
-  setTimeout(() => {
-    navigate("/signup")
-  }, 2000)
+  if (!isLoggedIn) {
+    setTimeout(() => {
+      navigate("/signup")
+    }, 2000)
 
-  return (
-    <div className="home-page">
-      <Container>
-        <h1
-          style={{ fontSize: "80px" }}
-          className="fade-in-out text-center text-white">
-          How are you feeling today?
-        </h1>
-      </Container>
-    </div>
-  )
+    return (
+      <div className="home-page">
+        <Container>
+          <h1
+            style={{ fontSize: "80px" }}
+            className="fade-in-out text-center text-white">
+            How are you feeling today?
+          </h1>
+        </Container>
+      </div>
+    )
+
+  }
+  else if(isLoggedIn && user){
+   navigate('/posts')
+  }
+  else if(isLoggedIn && therapist){
+    navigate('/users')
+  }
+
+
 }
 
 export default HomePage
