986e3e96f592e8794c2704f7a59b793a076e51ac
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2023-05-11 20:55:50 +0200

    therapist auth 2

--- src/pages/TherapistLogin.jsx
+++ src/pages/TherapistLogin.jsx
@@ -1,5 +1,104 @@
 import React from "react"
+import { useState, useContext } from "react"
+import axios from "axios"
+import { Link, useNavigate } from "react-router-dom"
+
+import { AuthContext } from "../context/auth.context"
+
+import Button from "react-bootstrap/Button"
+import Col from "react-bootstrap/Col"
+import Form from "react-bootstrap/Form"
+import InputGroup from "react-bootstrap/InputGroup"
+import Row from "react-bootstrap/Row"
+import { Container } from "react-bootstrap"
+
+const API_URL = "http://localhost:5006"
 
 export default function TherapistLogin() {
-  return <div>Therapislogin</div>
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [errorMessage, setErrorMessage] = useState(undefined)
+  const [validated, setValidated] = useState(false)
+  const navigate = useNavigate()
+  const { storedToken, authenticateUser } = useContext(AuthContext)
+
+  const handleEmail = (e) => setEmail(e.target.value)
+  const handlePassword = (e) => setPassword(e.target.value)
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault()
+    const requestBody = { email, password }
+
+    axios
+      .post(`${API_URL}/therapistlogin`, requestBody)
+      .then((response) => {
+        // Request to the server's endpoint `/auth/login` returns a response
+        // with the JWT string ->  response.data.authToken
+        console.log("JWT token", response.data.authToken)
+        storedToken(response.data.authToken)
+        authenticateUser()
+        // navigate("/users") // <== ADD
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message
+        setErrorMessage(errorDescription)
+      })
+  }
+
+  return (
+    <Container className="TherapistLoginPage">
+      <h1>Login and follow your patients</h1>
+
+      <Form
+        style={{ padding: "40px", justifyContent: "center", display: "flex", flexDirection: "column" }}
+        noValidate
+        validated={validated}
+        onSubmit={handleLoginSubmit}>
+        <Row
+          className="mb-3"
+          width="80vw">
+          <Form.Group
+            as={Col}
+            md="4"
+            controlId="validationCustom02">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              required
+              placeholder="Your Email"
+              type="email"
+              name="email"
+              value={email}
+              onChange={handleEmail}
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group
+            as={Col}
+            md="4"
+            controlId="validationCustomUsername">
+            <Form.Label>Password</Form.Label>
+            <InputGroup hasValidation>
+              <Form.Control
+                placeholder="Password"
+                aria-describedby="inputGroupPrepend"
+                required
+                type="password"
+                name="password"
+                value={password}
+                onChange={handlePassword}
+              />
+              <Form.Control.Feedback type="invalid">Please choose a username.</Form.Control.Feedback>
+            </InputGroup>
+          </Form.Group>
+        </Row>
+        <Button
+          variant="dark"
+          type="submit">
+          Log in
+        </Button>
+      </Form>
+
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+    </Container>
+  )
 }
