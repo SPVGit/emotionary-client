58750c0b5441a00b5a8d05a2d98098468adc64cb
Vanya Mark <markova.vanya@gmail.com>
2023-05-13 13:58:55 +0200

    activity working crud

--- src/pages/SinglePostPage.jsx
+++ src/pages/SinglePostPage.jsx
@@ -24,6 +24,7 @@ const SinglePostPage = (props) => {
       })
       .then((response) => {
         const singlePost = response.data;
+      console.log('single post activity', singlePost.activities)
         console.log("response.data", response.data);
 
         setPost(singlePost);
@@ -64,159 +65,11 @@ const SinglePostPage = (props) => {
       })
 
         console.log('monkey')
-  navigate("/posts");
+ navigate("/posts");
 
  //  .catch((err) => console.log(err))
   }
 
-
-  /* Diogo's delete code on todos-final-boss 
-  
-  import React, { useEffect, useState, useContext } from 'react'
-import axios from 'axios'
-import Todo from './../../components/Todo'
-import { AuthContext } from '../../context/auth.context'
-
-function TodosPage() {
-
-  const [todos, setTodos] = useState([])
-
-  const { user } = useContext(AuthContext)
-
-  const updateTodoAPI = (updatedTodo) => {
-    // update the back-end
-    axios.put(`http://localhost:5005/todos/`, {todos, updatedTodo},
-      { headers: { Authorization: `Bearer ${storedToken}` } })
-      .then(response => {
-        if (response.data) {
-
-          
-            console.log(response.data)
-          
-
-        }
-      })
-  }
-
-  const updateTodoArray = (value, id) => {
-
-    let updatedTodo
-
-    // update the front-end
-    setTodos(todos => {
-      return todos.map(todo => {
-        if (todo._id == id) {
-          todo.title = value
-          updatedTodo = todo
-        }
-        return todo
-      })
-    })
-    
-    updateTodoAPI(updatedTodo)
-
-
-  }
-
-
-  const handleSubmit = (event) => {
-
-    event.preventDefault()
-
-    const form = event.target
-
-    const title = form.title.value
-    const description = form.description.value
-    const deadline = form.deadline.value
-
-    const newTodo = { title, description, deadline, userId: user._id }
-
-    console.log(newTodo)
-
-    axios.post(`http://localhost:5005/todos/`,
-      newTodo,
-      { headers: { Authorization: `Bearer ${storedToken}` } })
-      .then(response => {
-        if (response.data) {
-
-          const newTodos = response.data
-          setTodos(newTodos)
-
-        }
-      })
-  }
-
-  const storedToken = localStorage.getItem('authToken');
-  useEffect(() => {
-
-    axios.get(`http://localhost:5005/user/${user._id}/todos/`,
-      { headers: { Authorization: `Bearer ${storedToken}` } })
-      .then(response => {
-        const todos = response.data
-        setTodos(todos)
-      })
-
-  }, [])
-
-  const deleteTodo = (id) => {
-
-    // delete from the front-end
-    setTodos(todos => {
-
-      const newTodos = todos.filter(todo => {
-        return todo._id !== id
-      })
-
-      return newTodos
-
-    })
-
-    axios.delete(`http://localhost:5005/todos/${id}/`,
-      { headers: { Authorization: `Bearer ${storedToken}` } })
-      .then(response => {
-
-        const deletedTodo = response.data
-
-        if (deletedTodo._id !== id) {
-          throw `Something went wrong.`
-        }
-
-      }).catch(err => {
-        console.error(err)
-      })
-
-  }
-
-  return (
-    <div>
-
-      <h1> Todos List </h1>
-
-      {
-        !todos.length && <div> No Todos Found! 😡 </div>
-      }
-
-      <div id='todo-list'>
-        {todos.map(todo => (
-          <Todo key={todo._id} todo={todo} deleteTodo={deleteTodo} updateTodoArray={updateTodoArray} />
-        ))}
-      </div>
-
-      <form onSubmit={handleSubmit}>
-
-        <input type='text' name='title' />
-        <input type='text' name='description' />
-        <input type='date' name='deadline' />
-
-        <input type='submit' />
-      </form>
-
-    </div>
-  )
-}
-
-export default TodosPage
-  */
 
   return (
     <div className="SinglePostPage">
@@ -227,6 +80,7 @@ export default TodosPage
           <p>{post.description}</p>
           <p>{post.rating}</p>
           {post.activities.map((activity) => (
+            activity && (
             <ListGroup key={activity._id}>
               <ListGroup.Item>{activity.title}</ListGroup.Item>
               <ListGroup.Item>{activity.level}</ListGroup.Item>
@@ -240,7 +94,7 @@ export default TodosPage
               <Link to={`/posts/${postId}/edit/${activity._id}`}>
                 <button>Edit activity</button>
               </Link> 
-            </ListGroup>
+            </ListGroup>)
           ))}
         </div>
       )}
