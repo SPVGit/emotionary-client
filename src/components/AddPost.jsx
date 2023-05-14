41415348dd0f939433262fa5e79662f69e8173df
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2023-05-14 22:41:58 +0200

    styling

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -3,13 +3,17 @@ import axios from "axios"
 import { useContext } from "react"
 import { AuthContext } from "../context/auth.context"
 import { useNavigate } from "react-router-dom"
+import Container from "react-bootstrap/Container"
+import Form from "react-bootstrap/Form"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Button from "react-bootstrap/Button"
 
 const API_URL = `http://localhost:${process.env.REACT_APP_API_URL}`
 console.log(process.env.REACT_APP_API_URL)
 
 const AddPost = () => {
   const { user } = useContext(AuthContext)
-
   const myDay = new Date()
 
   const [newPost, setNewPost] = useState({
@@ -62,61 +66,166 @@ const AddPost = () => {
   }
 
   return (
-    <div>
-      <h3>Add Emotion</h3>
+    <Container cassName="mb-3">
+      <Form
+        style={{ padding: "40px", justifyContent: "center", display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit}>
+        <h3>Add Emotion</h3>
+        <Row className="mb-3 mt-3">
+          <Form.Group
+            as={Col}
+            md="4"
+            controlId="validationCustom01">
+            <Form.Label>Date</Form.Label>
+            <Form.Control
+              required
+              type="date"
+              name="date"
+              value={newPost.date}
+              onChange={handleChange}
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group
+            as={Col}
+            md="4"
+            controlId="validationCustom02">
+            <Form.Label>Emotion:</Form.Label>
+            <Form.Select
+              name="emotion"
+              value={newPost.emotion}
+              onChange={handleChange}
+              required>
+              <option value="happy">Happy</option>
+              <option value="in-love">In Love</option>
+              <option value="excited">Excited</option>
+              <option value="satisfied">Satisfied</option>
+              <option value="calm">Calm</option>
+              <option value="sad">Sad</option>
+              <option value="anxious">Anxious</option>
+              <option value="hurt">Hurt</option>
+              <option value="embarrassed">Embarrassed</option>
+              <option value="depressed">Depressed</option>
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Select>
+          </Form.Group>
 
-      <form onSubmit={handleSubmit}>
-        <label>Date</label>
-        <input
-          type="date"
-          name="date"
-          value={newPost.date}
-          onChange={handleChange}
-          required
-        />
-        <label>Emotion:</label>
-        <select
-          name="emotion"
-          value={newPost.emotion}
-          onChange={handleChange}
-          required>
-          <option value="happy">Happy</option>
-          <option value="in-love">In Love</option>
-          <option value="excited">Excited</option>
-          <option value="satisfied">Satisfied</option>
-          <option value="calm">Calm</option>
-          <option value="sad">Sad</option>
-          <option value="anxious">Anxious</option>
-          <option value="hurt">Hurt</option>
-          <option value="embarrassed">Embarrassed</option>
-          <option value="depressed">Depressed</option>
-        </select>
+          <Form.Group
+            as={Col}
+            md="4"
+            controlId="validationCustom03">
+            <Form.Label>Emotion Intensity:</Form.Label>
+            <Form.Select
+              name="rating"
+              value={newPost.rating}
+              onChange={handleChange}
+              required>
+              <option value="1">✮ </option>
+              <option value="2">✮✮</option>
+              <option value="3">✮✮✮</option>
+              <option value="4">✮✮✮✮</option>
+              <option value="5">✮✮✮✮✮</option>
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Select>
+          </Form.Group>
 
-        <label>Emotion Intensity:</label>
-        <select
-          name="rating"
-          value={newPost.rating}
-          onChange={handleChange}
-          required>
-          <option value="1">✮ </option>
-          <option value="2">✮✮</option>
-          <option value="3">✮✮✮</option>
-          <option value="4">✮✮✮✮</option>
-          <option value="5">✮✮✮✮✮</option>
-        </select>
+          <Form.Group
+            className="mt-4"
+            as={Col}
+            controlId="validationCustom04">
+            <Form.Label>What made you feel that way?</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              type="text"
+              name="description"
+              value={newPost.description}
+              onChange={handleChange}
+            />
+          </Form.Group>
+        </Row>
 
-        <label>What made you feel that way?</label>
-        <textarea
-          type="text"
-          name="description"
-          value={newPost.description}
-          onChange={handleChange}
-        />
-
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+        <Button
+          className="shadow"
+          variant="dark"
+          type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
   )
 }
 
 export default AddPost
+
+// <Form noValidate validated={validated} onSubmit={handleSubmit}>
+// <Row className="mb-3">
+//   <Form.Group as={Col} md="4" controlId="validationCustom01">
+//     <Form.Label>First name</Form.Label>
+//     <Form.Control
+//       required
+//       type="text"
+//       placeholder="First name"
+//       defaultValue="Mark"
+//     />
+//     <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+//   </Form.Group>
+//   <Form.Group as={Col} md="4" controlId="validationCustom02">
+//     <Form.Label>Last name</Form.Label>
+//     <Form.Control
+//       required
+//       type="text"
+//       placeholder="Last name"
+//       defaultValue="Otto"
+//     />
+//     <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+//   </Form.Group>
+//   <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+//     <Form.Label>Username</Form.Label>
+//     <InputGroup hasValidation>
+//       <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+//       <Form.Control
+//         type="text"
+//         placeholder="Username"
+//         aria-describedby="inputGroupPrepend"
+//         required
+//       />
+//       <Form.Control.Feedback type="invalid">
+//         Please choose a username.
+//       </Form.Control.Feedback>
+//     </InputGroup>
+//   </Form.Group>
+// </Row>
+// <Row className="mb-3">
+//   <Form.Group as={Col} md="6" controlId="validationCustom03">
+//     <Form.Label>City</Form.Label>
+//     <Form.Control type="text" placeholder="City" required />
+//     <Form.Control.Feedback type="invalid">
+//       Please provide a valid city.
+//     </Form.Control.Feedback>
+//   </Form.Group>
+//   <Form.Group as={Col} md="3" controlId="validationCustom04">
+//     <Form.Label>State</Form.Label>
+//     <Form.Control type="text" placeholder="State" required />
+//     <Form.Control.Feedback type="invalid">
+//       Please provide a valid state.
+//     </Form.Control.Feedback>
+//   </Form.Group>
+//   <Form.Group as={Col} md="3" controlId="validationCustom05">
+//     <Form.Label>Zip</Form.Label>
+//     <Form.Control type="text" placeholder="Zip" required />
+//     <Form.Control.Feedback type="invalid">
+//       Please provide a valid zip.
+//     </Form.Control.Feedback>
+//   </Form.Group>
+// </Row>
+// <Form.Group className="mb-3">
+//   <Form.Check
+//     required
+//     label="Agree to terms and conditions"
+//     feedback="You must agree before submitting."
+//     feedbackType="invalid"
+//   />
+// </Form.Group>
+// <Button type="submit">Submit form</Button>
+// </Form>
