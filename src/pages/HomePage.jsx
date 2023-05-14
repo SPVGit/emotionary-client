f50e6ce6e3d50fabe8f0d39700f3d6865b96937b
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2023-05-14 17:12:36 +0200

    styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from "react"
 import { Container } from "react-bootstrap"
 import { useNavigate } from "react-router-dom"
-import { useContext } from "react";
-import { AuthContext } from "../context/auth.context";
+import { useContext } from "react"
+import { AuthContext } from "../context/auth.context"
 
 const HomePage = () => {
-  const { isLoggedIn,user,therapist} = useContext(AuthContext);
+  const { isLoggedIn, user, therapist } = useContext(AuthContext)
   const navigate = useNavigate()
 
   if (!isLoggedIn) {
@@ -16,24 +16,15 @@ const HomePage = () => {
     return (
       <div className="home-page">
         <Container>
-          <h1
-            style={{ fontSize: "80px" }}
-            className="fade-in-out text-center text-white">
-            How are you feeling today?
-          </h1>
+          <h1 className="fade-in-out text-center text-white">How are you feeling today?</h1>
         </Container>
       </div>
     )
-
+  } else if (isLoggedIn && user) {
+    navigate("/posts")
+  } else if (isLoggedIn && therapist) {
+    navigate("/users")
   }
-  else if(isLoggedIn && user){
-   navigate('/posts')
-  }
-  else if(isLoggedIn && therapist){
-    navigate('/users')
-  }
-
-
 }
 
 export default HomePage
